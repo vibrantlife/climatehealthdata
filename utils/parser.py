9eef4335f9c572f77a433ef86e88faa690636aa8
Impossible <scottan@gmail.com>
2015-06-06 15:51:22 -0700

    Changed "csvfile" to a more accurate "datfile"

--- utils/parser.py
+++ utils/parser.py
@@ -1,8 +1,8 @@
 import csv
 import json
 
-with open('data/t17_02.dat','rb') as csvfile:
-	datreader = csv.reader(csvfile,delimiter=' ',quoting=csv.QUOTE_NONE)
+with open('data/t17_02.dat','rb') as datfile:
+	datreader = csv.reader(datfile,delimiter=' ',quoting=csv.QUOTE_NONE)
 	with open('data/t17_02.json','w') as jsonfile:
 		items = []
 		for row in datreader:
